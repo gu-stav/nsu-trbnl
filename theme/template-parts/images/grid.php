3ae46292838896099e7cbe4d32ce04e45c809196
Gustav Pursche <pursche@posteo.de>
2019-03-14 16:52:54 +0100

    Grid: fix item display for 2 and 4 items

--- theme/template-parts/images/grid.php
+++ theme/template-parts/images/grid.php
@@ -1,6 +1,12 @@
-<ul class="grid">
-  <?php while (have_rows('images') ) : the_row(); ?>
-    <li>
+<?php
+
+  $images = get_field('images');
+
+?>
+
+<ul class="grid grid--items-<?php echo sizeof($images); ?>">
+  <?php while (have_rows('images')) : the_row(); ?>
+    <li class="grid__item">
       <?php get_template_part('template-parts/images/image'); ?>
     </li>
   <?php endwhile; ?>
--- theme/template-parts/videos/grid.php
+++ theme/template-parts/videos/grid.php
@@ -1,6 +1,12 @@
-<ul class="grid">
+<?php
+
+  $videos = get_field('videos');
+
+?>
+
+<ul class="grid grid--items-<?php echo sizeof($videos); ?>">
   <?php while (have_rows('videos') ) : the_row(); ?>
-    <li>
+    <li class="grid__item">
       <?php get_template_part('template-parts/videos/video'); ?>
     </li>
   <?php endwhile; ?>
