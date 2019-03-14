bacc3e590cebae4032cc67430fcf96378bd25c19
Gustav Pursche <pursche@posteo.de>
2019-03-14 17:19:44 +0100

    Intro: allow portrait images

--- theme/blocks/intro.php
+++ theme/blocks/intro.php
@@ -4,19 +4,32 @@
   $title = get_field('title');
   $subtitle = get_field('subtitle');
 
+  $image_src = wp_get_attachment_image_src($image, false);
+  $image_height = $image_src[2];
+  $image_width = $image_src[1];
+
+  if ($image_width > $image_height) {
+    $image_orientation = 'landscape';
+  } else {
+    $image_orientation = 'portrait';
+  }
+
 ?>
 
-<div class="intro">
-  <?php echo wp_get_attachment_image($image, false, false, [
-    'class' => 'image intro__image'
-  ]); ?>
+<div class="intro intro--orientation-<?php echo $image_orientation; ?>">
+
+  <div class="intro__image-container">
+    <?php echo wp_get_attachment_image($image, false, false, [
+      'class' => 'image intro__image'
+    ]); ?>
+  </div>
 
   <div class="intro__content-container constraint">
     <h1 class="headline headline--as-h1">
       <?php echo $title ?>
 
       <?php if($subtitle): ?>
-        <small class="headline headline--as-h2">
+        <small class="intro__subtitle headline headline--as-h2">
           <?php echo $subtitle; ?>
         </small>
       <?php endif; ?>
