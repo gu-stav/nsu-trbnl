a6023f770b23aa97a0538da7edf0ed8994f96b39
Gustav Pursche <pursche@posteo.de>
2019-03-20 12:59:02 +0100

    Enhance tinyMCE toolbars

--- theme/lib/admin.php
+++ theme/lib/admin.php
@@ -12,14 +12,31 @@
   }
 
   // redirect to pages tab after login
-  function loginRedirect( $redirect_to, $request, $user ){
+  function admin_login_redirect( $redirect_to, $request, $user ){
     if(is_array( $user->roles)) {
         return '/wp-admin/edit.php?post_type=page';
     }
   }
 
-  add_filter('login_redirect', 'loginRedirect', 10, 3);
+  function admin_toolbar_cleanup($toolbars) {
+    var_dump($toolbars);
+
+    $toolbars['Richtext'] = array();
+    $toolbars['Richtext'][1] = array('formatselect', 'bold', 'italic', 'link', 'undo', 'redo');
+
+    return $toolbars;
+  }
+
+  function admin_tinymce_formatselect($settings) {
+    $settings['block_formats'] = 'Absatz=p;Überschrift(1)=h2;Überschrift(2)=h3;Überschrift(3)=h4';
+
+    return $settings;
+  }
+
+  add_filter('login_redirect', 'admin_login_redirect', 10, 3);
   add_action('admin_menu', 'admin_cleanup_menu');
+  add_filter('acf/fields/wysiwyg/toolbars' , 'admin_toolbar_cleanup');
+  add_filter('tiny_mce_before_init', 'admin_tinymce_formatselect');
 
   add_editor_style(get_template_directory_uri() . '/style-editor.css');
   add_editor_style(get_template_directory_uri() . '/.build/index.css');
