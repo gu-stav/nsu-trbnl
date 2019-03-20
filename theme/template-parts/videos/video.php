37a54f1c52d7d9b718f388fb054593b81466958d
Gustav Pursche <pursche@posteo.de>
2019-03-20 12:10:38 +0100

    Video: add play button

--- theme/template-parts/videos/video.php
+++ theme/template-parts/videos/video.php
@@ -9,15 +9,15 @@
     <?php echo wp_get_attachment_image($image, 'half-width', false, [
       'class' => 'image'
     ]); ?>
-  </a>
 
-  <?php if($caption): ?>
     <figcaption class="figure__caption">
       <div class="figure__caption-container">
         <?php echo $caption; ?>
       </div>
 
-      <strong>PLAY</strong>
+      <svg aria-hidden="true" focusable="false" class="figure__play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path fill="currentColor" d="M371.7 238l-176-107c-15.8-8.8-35.7 2.5-35.7 21v208c0 18.4 19.8 29.8 35.7 21l176-101c16.4-9.1 16.4-32.8 0-42zM504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256z"></path>
+      </svg>
     </figcaption>
-  <?php endif; ?>
+  </a>
 </figure>
