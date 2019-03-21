c6d90db23b53b10b9f80b62be2f035edabd1b338
Gustav Pursche <pursche@posteo.de>
2019-03-21 18:42:56 +0100

    Teaser: render video grid

--- theme/blocks/teaser.php
+++ theme/blocks/teaser.php
@@ -3,8 +3,12 @@
   $text = get_field('text');
   $target = get_field('target');
   $button_label = get_field('button_label');
+
   $images = get_field('images');
-  $has_images = sizeof($images) > 1 || ( sizeof($images) == 1 && $images[0]['image'] != false );
+  $has_images = $images && (sizeof($images) > 1 || ( sizeof($images) == 1 && $images[0]['image'] != false ));
+
+  $videos = get_field('videos');
+  $has_videos = $videos && (sizeof($videos) > 1 || ( sizeof($videos) == 1 && $videos[0]['youtube_video_id'] != false ));
 
 ?>
 
@@ -24,6 +28,12 @@
   <?php if ($has_images): ?>
     <div class="constraint constraint--wide">
       <?php get_template_part('template-parts/images/grid'); ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($has_videos): ?>
+    <div class="constraint constraint--wide">
+      <?php get_template_part('template-parts/videos/grid'); ?>
     </div>
   <?php endif; ?>
 
