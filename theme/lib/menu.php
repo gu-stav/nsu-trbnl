b25ba66b1717733b3572f90f620b47685070ff07
Gustav Pursche <pursche@posteo.de>
2019-03-21 18:53:54 +0100

    Footer: render icon in social media menu

--- theme/lib/menu.php
+++ theme/lib/menu.php
@@ -15,6 +15,21 @@
     ]);
   }
 
+  function menu_social_media_icons( $items ) {
+    foreach( $items as &$item ) {
+      $icon = get_field( 'icon', $item );
+
+      if( $icon ) {
+        $item->title = '<span class="menu-item__icon">' . $icon . '</span>' .
+                       '<span class="menu-item__hidden-label">' . $item->title . '</span>';
+      }
+    }
+
+    return $items;
+  }
+
   add_action( 'init', 'menu_register' );
   add_action( 'widgets_init', 'menu_footer_widgets' );
+
+  add_filter( 'wp_nav_menu_objects', 'menu_social_media_icons', 10, 2 );
 ?>
